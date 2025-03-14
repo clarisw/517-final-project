e510c3eaa1631c59a6b5bead5807ea2b918bbced
Rustin Soraki <rustin.soraki@gmail.com>
2025-03-14 10:36:27 -0700

    Generate Even More Data

--- process_data.sh
+++ process_data.sh
@@ -5,8 +5,8 @@ model_name="meta-llama/Llama-2-7b-hf"
 
 # total_idx=200
 
-start_idx=0
-end_idx=25
+# start_idx=0
+# end_idx=25
 
 # start_idx=25
 # end_idx=50
@@ -14,14 +14,15 @@ end_idx=25
 # start_idx=50
 # end_idx=75
 
-# start_idx=75
-# end_idx=100
+start_idx=75
+end_idx=100
 
 # race="Asian"
 # race="Black"
-race="Hispanic"
+# race="Hispanic"
+race="White"
 
-CUDA_VISIBLE_DEVICES=1 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python process_data.py \
+CUDA_VISIBLE_DEVICES=0 python process_data.py \
     --model-name $model_name \
     --start-idx $start_idx \
     --end-idx $end_idx \

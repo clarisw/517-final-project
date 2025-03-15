e8769dd8dcd55352f2648a35972c95ff3f7e161a
Evan Lam <45635779+IMlame@users.noreply.github.com>
2025-03-14 19:07:01 -0700

    Update generate_heatmaps.py

--- generate_heatmaps.py
+++ generate_heatmaps.py
@@ -1,7 +1,6 @@
 import seaborn as sns
 import pandas as pd
 
-
 names_df = pd.read_csv("names.csv")
 
 # race -> name -> percent female at birth
@@ -15,6 +14,7 @@ for index, row in names_df.iterrows():
     names_map[race][name] = percent
 
 results_df = pd.read_csv("accuracy_results.csv")
+
 results_df["percent 1"] = results_df.apply(lambda x: names_map[x["race"]][x["name1"]], axis=1)
 results_df["percent 2"] = results_df.apply(lambda x: names_map[x["race"]][x["name2"]], axis=1)
 
@@ -24,25 +24,26 @@ results_df["bucket 2"] = pd.cut(x=results_df['percent 2'], bins=[-1, 2, 5, 10, 2
 # grouped_df = results_df.groupby(by=["bucket 1", "bucket 2"], as_index=False)["accuracy"].mean()
 grouped_df = results_df.groupby(by=["race", "bucket 1", "bucket 2"], as_index=False)[["tp","tn","fp","fn","total"]].sum()
 
-grouped_df.sort_values(["bucket 1","bucket 2"], ascending=True)
-
 grouped_df["precision"] = (grouped_df["tp"] / (grouped_df["tp"] + grouped_df["fp"])).round(2)
 grouped_df["accuracy"] = ((grouped_df["tp"] + grouped_df["tn"]) / grouped_df["total"]).round(2)
 grouped_df["recall"] = (grouped_df["tp"] / (grouped_df["tp"] + grouped_df["fn"])).round(2)
 grouped_df["f1"] = (2 * (grouped_df["precision"] * grouped_df["recall"]) / (grouped_df["precision"] + grouped_df["recall"])).round(2)
 
+import matplotlib.pyplot as plt
 def generate_heatmap(df, value, race):
     heatmap_df = pd.pivot_table(grouped_df[grouped_df["race"] == race], values=value, index='bucket 1',
                        columns='bucket 2')
     cmap = sns.light_palette("seagreen", as_cmap=True)
+    plt.figure()
     ax = sns.heatmap(data=heatmap_df, cmap="Greens", annot=True)
     ax.set_xlabel("% Female")
     ax.set_ylabel("% Female")
     ax.set_title(f"{race} {value}")
+    plt.savefig(f"{race}-{value}.png")
+    plt.plot()
 
-# Run this for combinations
-generate_heatmap(grouped_df, "recall", "Black")
-# generate_heatmap(grouped_df, "recall", "Asian")
-# generate_heatmap(grouped_df, "accuracy")
-# generate_heatmap(grouped_df, "recall")
-# generate_heatmap(grouped_df, "f1")
+races = ["Asian", "Black", "Hispanic","White"]
+metric_types = ["recall", "accuracy", "precision","f1"]
+for race in races:
+    for metric in metric_types:
+        generate_heatmap(grouped_df, metric, race)
